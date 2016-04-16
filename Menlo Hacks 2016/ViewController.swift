1c18645b4485c9df21879eb20068c3a291815ff3
Dhruv Mangtani <turbodhruv@gmail.com>
2016-04-16 12:11:54 -0700

    Initial Commit

---
+++ Menlo Hacks 2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Menlo Hacks 2016
+//
+//  Created by Dhruv Mangtani on 4/16/16.
+//  Copyright © 2016 dhruv.mangtani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
